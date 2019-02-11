2e10b40cee839fe97eac36e3666536b0aef11e14
Marcalzotto <marcos.scalzotto24@gmail.com>
2019-02-11 18:06:28 -0300

    subimos lo viste hasta la clase 47

--- callbacks/main.js
+++ callbacks/main.js
@@ -11,4 +11,26 @@
 //todo el proceso de gestion de los eventos de sistema es asincronico,
 //porque se ejecuta simultaneamente con lo que este ejecutando v8
 //la arquitectura controlada por eventos sin bloqueo ayuda a node a ser muy performante
-//
+//buffer:es un lugar en memoria donde se pueden almacenar datos en forma temporalç
+//para pasarlos a otro lugar, el buffer esta limitado en tamaño.
+//Stream o secuencia: Trozos de informacion que se convinan en uno solo
+//El buffer almacena partes de los datos transmitidos que luego pasan al proceso o procesos 
+//finales, luego almacena otra parte hasta que los procesos reciben el 100% de los datos enviados.
+//la cpu procesa la informacion en numero binarios (0 y 1)
+//un juego de caracteres permite representar caracteres como numeros, ASCII y unicode son juegos
+// de caracteres
+//Codificacion de caracteres: cuantos bits estamos usando para almacenar un numero en binario,
+//de los asignados a un carcter por un determinado juego de cartacteres
+
+var bufer = new Buffer('Almacenamos una frase en el buffer','utf8');
+
+console.log(bufer); //se muestra lo almacenado en el buffer en exadecimal, internamente
+//los datos se alamcenan en numero binario
+console.log(bufer.toString());//muestra el contenido del buffer en formato de string
+console.log(bufer.toJSON());// muestra el contenido del buffer en formato JSON
+console.log('-------------\n');
+console.log(bufer[2]);
+
+bufer.write('Guardamoss');
+console.log('---------------------\n');
+console.log(bufer.toString());
