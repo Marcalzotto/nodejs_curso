33978b3de1cb044d9a3301a306d426cbbafd2ec7
Marcalzotto <marcos.scalzotto24@gmail.com>
2019-02-18 23:25:30 -0300

    subimos algo mas sobre transmisiones

--- transmisiones/main.js
+++ transmisiones/main.js
@@ -1,6 +1,6 @@
-//las transmiciones, es la informacion se divide parte de un tamaño x
+//las transmiciones, es la informacion que se divide parte de un tamaño x
 // y se envia de un punto a otro.
-//la transimision hereda del emisor de eventos
+//la transimision(Class Stream) hereda del emisor de eventos(class EventEmitter)
 //existen 5 tipos de transmisiones:
 //Legible(Readable) solo puede leer los datos que estan llegando, no puede
 //enviar datos de regreso
@@ -11,6 +11,16 @@
 //PassThrough: 
 //Los tipos de transmisiones heredan de Stream, que a su vez hereda de EventEmitter
 //Clase abstracta: una clase que se usa como base, un tipo de constructor
-//con el cual no se trabaja en forma directa pero si hereda de este.
-//los clases existentes para los diferentes tipo de Streams son abstractas
+//con el cual no se trabaja en forma directa pero si se hereda de este.
+//los clases existentes para los diferentes tipos de Streams son abstractas
 //para trabajar con ellas se debe crear un Stream personalizado que herede de ellas
+//el metodo createReadStream es un metodo del modulo fileSistem que devuelve una
+//clase ReadStream es tipo especial de transmision Legible(Readable)
+
+var fs = require('fs');
+
+var readable = fs.createReadStream(__dirname + '/lorem.txt',{encoding : 'utf8'});
+
+readable.on('data',function(chunk){
+    console.log(chunk.length);
+});
